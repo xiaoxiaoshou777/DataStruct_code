5991c70edc79b7d9bd152402d3fc74f1a76501ac
xiaoxiaoshou777 <xiaoxiaoshoushiwo@163.com>
2017-07-11 21:07:00 +0800

    SeqList

---
+++ SeqList/SeqList.hpp
@@ -0,0 +1,193 @@
+#include<assert.h>
+
+template<class T>
+class SeqList
+{
+public:
+	SeqList()
+		:_data(0)
+		, _size(0)
+		, _capacity(0)
+	{
+		_CheckExpand(7);
+	}
+
+	SeqList(const SeqList& v)
+		:_size(v._size)
+		, _capacity(v._capacity)
+	{
+		_data = new T[_capacity];
+
+		// 此处需要类型萃取后进行处理，对自动义类型进行依次的拷贝
+
+		// 此处可使用类型萃取进行优化
+
+		if (TypeTraits<T>::__IsPODType().Get())
+			//memcpy(tmp, _data, sizeof(T)*_size);
+
+			// T->string
+		for (size_t i = 0; i < _size; ++i)
+		{
+			tmp[i] = _data[i];  //string operator=
+		}
+	}
+
+	//SeqList& operator=(const SeqList& v)
+	//{
+	//	if (&v != this)
+	//	{
+	//	/*	_Destory();
+	//		_size = v._size;
+	//		_capacity = v._capacity;
+	//		_data = new T[_capacity];
+	//
+	//		// 此处需要类型萃取后进行处理，对自动义类型进行依次的拷贝
+	//		memcpy(_data, v._data, sizeof(_size)*_size);
+	//	}
+	//	*/
+	//	return *this;
+	//}
+
+	void Swap(SeqList& tmp)
+	{
+		std::swap(_data, tmp._data);
+		std::swap(_capacity, tmp._capacity);
+		std::swap(_size, tmp._size);
+	}
+
+	SeqList& operator=(SeqList s)
+	{
+		Swap(s);
+		return *this;
+	}
+
+	virtual ~SeqList()
+	{
+		_Destory();
+	}
+
+protected:
+	inline void _CheckExpand(size_t size)
+	{
+		if (_size < _capacity)
+			return;
+
+		_capacity = size;
+		T* tmp = new T[_capacity];
+		if (_data)
+		{
+			// 此处可使用类型萃取进行优化
+			//memcpy(tmp, _data, sizeof(T)*_size);
+			for (size_t i = 0; i < _size; ++i)
+			{
+				tmp[i] = _data[i];
+			}
+
+			delete[] _data;
+		}
+
+		_data = tmp;
+	}
+
+	void _Destory()
+	{
+		if (_data)
+		{
+			delete[] _data;
+		}
+
+		_capacity = 0;
+		_size = 0;
+	}
+
+public:
+	// size > capacity 时，将顺序表的容量增加到size
+	void Reserve(size_t size)
+	{
+		_CheckExpand(size);
+	}
+
+	void PushBack(const T& x)
+	{
+		if (_capacity == _size)
+		{
+			_CheckExpand(2 * _capacity);
+		}
+
+		_data[_size++] = x;
+	}
+
+	void PopBack()
+	{
+		if (_size > 0)
+			--_size;
+	}
+
+	// 在pos位置上插入一个数据
+	void Insert(size_t pos, const T& x)
+	{
+		assert(pos <= _size);
+
+		if (_capacity == _size)
+		{
+			_CheckExpand(2 * _capacity);
+		}
+
+		size_t last = _size;
+		while (pos < last)
+		{
+			_data[last] = _data[last - 1];
+			--last;
+		}
+
+		_data[pos] = x;
+		++_size;
+	}
+
+	void Earse(size_t pos)
+	{
+		assert(pos < _size);
+
+		// 注意这里的边界条件
+		size_t begin = pos;
+		while (begin < _size - 1)
+		{
+			_data[begin] = _data[begin + 1];
+			++begin;
+		}
+
+		--_size;
+	}
+
+	T& operator[](size_t pos)
+	{
+		assert(pos < _size);
+
+		return _data[pos];
+	}
+
+	void Clear()
+	{
+		_size = 0;
+	}
+
+	size_t GetSize()
+	{
+		return _size;
+	}
+
+	void Print()
+	{
+		for (size_t i = 0; i < _size; ++i)
+		{
+			cout << _data[i] << " ";
+		}
+
+		cout << endl;
+	}
+
+private:
+	T*			_data;			// 数据块指针
+	size_t		_size;			// 数据的个数
+	size_t		_capacity;		// 容量
+};
---
+++ SeqList/Test.cpp
@@ -0,0 +1,116 @@
+﻿#include<iostream>
+using namespace std;
+
+#include"SeqList.hpp"
+
+// ²âÊÔPushBack
+void Test1()
+{
+	SeqList<int> v1;
+	v1.PushBack(1);
+	v1.PushBack(2);
+	v1.PushBack(3);
+	v1.PushBack(4);
+
+	v1.Print();
+}
+
+// ²âÊÔInsert
+void Test2()
+{
+	SeqList<int> v1;
+	v1.Insert(0, 1);
+	v1.Insert(1, 2);
+	v1.Insert(2, 3);
+	v1.Insert(3, 4);
+
+	v1.Insert(0, 0);
+
+	v1.Print();
+}
+
+// ²âÊÔPopBack
+void Test3()
+{
+	SeqList<int> v1;
+	v1.Insert(0, 1);
+	v1.Insert(1, 2);
+	v1.Insert(2, 3);
+	v1.Insert(3, 4);
+
+	v1.Print();
+
+	v1.PopBack();
+
+	v1.Print();
+}
+
+// ²âÊÔEarse
+void Test4()
+{
+	SeqList<int> v1;
+	v1.Insert(0, 1);
+	v1.Insert(1, 2);
+	v1.Insert(2, 3);
+	v1.Insert(3, 4);
+
+	v1.Print();
+
+	v1.Earse(0);
+	v1.Earse(v1.GetSize() - 1);
+
+	v1.Print();
+}
+
+// ²âÊÔoperator[]
+void Test5()
+{
+	SeqList<int> v1;
+	v1.Insert(0, 1);
+	v1.Insert(1, 2);
+	v1.Insert(2, 3);
+	v1.Insert(3, 4);
+
+	v1.Print();
+
+	v1[0] = 0;
+	v1[1] = 1;
+	v1[2] = 2;
+	v1[3] = 3;
+
+	v1.Print();
+
+	v1.Clear();
+
+	v1.Print();
+}
+
+// ²âÊÔ¿½±´¹¹Ôì&operator=
+void Test6()
+{
+	SeqList<int> v1;
+	v1.PushBack(1);
+	v1.PushBack(2);
+	v1.PushBack(3);
+	v1.PushBack(4);
+
+	SeqList<int> v2 = v1;
+	v2.Print();
+
+	v1.PushBack(5);
+
+	v2 = v1;
+	v2.Print();
+}
+
+int main()
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+
+	return 0;
+}
